0d72476d1d645e1e7a9acb56056347a30f43533b
Fedya Levkin <f.levkin@gmail.com>
2023-03-31 01:45:35 +0300

    Solved y2020d01s1!

--- src/y2020/d01.rs
+++ src/y2020/d01.rs
@@ -1,8 +1,28 @@
 use crate::file_reader::*;
 
+fn solve_sample1() -> usize {
+    let input = get_string_from_file_please("ianda/2020/01s.txt");
+    let numbers: Vec<usize> = input.lines().map(|x| x.parse().unwrap()).collect();
+    for i in 0..numbers.len() {
+        for j in i..numbers.len() {
+            if numbers[i] + numbers[j] == 2020 {
+                return numbers[i] * numbers[j];
+            }
+        }
+    }
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_solve_sample1() {
+        let result = solve_sample1();
+        let expected = get_usize_from_file_please("ianda/2020/01sa1.txt");
+        assert_eq!(result, expected);
+    }
 
     #[test]
     fn test_file_reader_module_access() {

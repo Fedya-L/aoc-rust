8279f68b9598ae24440945368441e8d7b585fbce
Fedya Levkin <f.levkin@gmail.com>
2023-03-31 19:52:26 +0300

    Solved y2020d01t1

--- src/y2020/d01.rs
+++ src/y2020/d01.rs
@@ -1,7 +1,7 @@
 use crate::file_reader::*;
 
-fn solve_sample1() -> usize {
-    let input = get_string_from_file_please("ianda/2020/01s.txt");
+fn solve_task1(path: &str) -> usize {
+    let input = get_string_from_file_please(path);
     let numbers: Vec<usize> = input.lines().map(|x| x.parse().unwrap()).collect();
     for i in 0..numbers.len() {
         for j in i..numbers.len() {
@@ -19,8 +19,15 @@ mod tests {
 
     #[test]
     fn test_solve_sample1() {
-        let result = solve_sample1();
+        let result = solve_task1("ianda/2020/01s.txt");
         let expected = get_usize_from_file_please("ianda/2020/01sa1.txt");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_solve_task1() {
+        let result = solve_task1("ianda/2020/01i.txt");
+        let expected = get_usize_from_file_please("ianda/2020/01a1.txt");
         assert_eq!(result, expected);
     }
 

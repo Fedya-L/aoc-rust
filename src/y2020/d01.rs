e1d24497c85a97f6eba16c8d06236956c00c5e58
Fedya Levkin <f.levkin@gmail.com>
2023-04-02 00:42:48 +0300

    Implemeted a solution for y2020d01 using Iterator (is this too much? probably)

--- src/y2020/d01.rs
+++ src/y2020/d01.rs
@@ -67,6 +67,71 @@ fn solve_task_universal(
     }
 }
 
+fn solve_task_iterator(numbers: Vec<usize>, numbers_to_sum: usize, compare_to_num: usize) -> usize {
+    let iter = WeirdIterator::new(numbers.len(), numbers_to_sum);
+    for indexes in iter {
+        let to_sum: Vec<usize> = indexes.iter().map(|x| numbers[*x]).collect();
+        if to_sum.iter().sum::<usize>() == compare_to_num {
+            return to_sum.iter().fold(1, |acc, x| acc * x);
+        }
+    }
+    0
+}
+
+#[derive(Debug)]
+struct WeirdIterator {
+    chunk_length: usize,
+    max_values: Vec<usize>,
+    current_chunk: Vec<usize>,
+    done: bool,
+}
+
+impl WeirdIterator {
+    fn new(total_length: usize, chunk_length: usize) -> WeirdIterator {
+        let starting_chunk: Vec<usize> = (0..chunk_length).map(|x| x).collect();
+        let max_values: Vec<usize> = starting_chunk
+            .iter()
+            .enumerate()
+            .map(|(i, _)| total_length - (chunk_length - i))
+            .collect();
+
+        WeirdIterator {
+            chunk_length,
+            max_values,
+            current_chunk: starting_chunk,
+            done: false,
+        }
+    }
+}
+
+impl Iterator for WeirdIterator {
+    type Item = Vec<usize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+        let cc = self.current_chunk.clone();
+        for ii in (0..self.chunk_length).rev() {
+            let i = self.current_chunk[ii];
+            if i + 1 > self.max_values[ii] {
+                if ii == 0 {
+                    self.done = true;
+                    return None;
+                }
+                continue;
+            }
+            let mut incr_by = 1;
+            for iiu in ii..self.chunk_length {
+                self.current_chunk[iiu] = i + incr_by;
+                incr_by = incr_by + 1;
+            }
+            break;
+        }
+        return Some(cc);
+    }
+}
+
 fn get_numbers_from_file(path: &str) -> Vec<usize> {
     let input = get_string_from_file_please(path);
     input.lines().map(|x| x.parse().unwrap()).collect()
@@ -75,6 +140,32 @@ fn get_numbers_from_file(path: &str) -> Vec<usize> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_solve_task_iterator_sample() {
+        let numbers = get_numbers_from_file("ianda/2020/01s.txt");
+
+        let expected1 = get_usize_from_file_please("ianda/2020/01sa1.txt");
+        let expected2 = get_usize_from_file_please("ianda/2020/01sa2.txt");
+
+        let s1 = solve_task_iterator(numbers.clone(), 2, 2020);
+        let s2 = solve_task_iterator(numbers.clone(), 3, 2020);
+        assert_eq!(s1, expected1);
+        assert_eq!(s2, expected2);
+    }
+
+    #[test]
+    fn test_solve_task_iterator_input() {
+        let numbers = get_numbers_from_file("ianda/2020/01i.txt");
+
+        let expected1 = get_usize_from_file_please("ianda/2020/01a1.txt");
+        let expected2 = get_usize_from_file_please("ianda/2020/01a2.txt");
+
+        let s1 = solve_task_iterator(numbers.clone(), 2, 2020);
+        let s2 = solve_task_iterator(numbers.clone(), 3, 2020);
+        assert_eq!(s1, expected1);
+        assert_eq!(s2, expected2);
+    }
 
     #[test]
     fn test_solve_task_universal_sample() {

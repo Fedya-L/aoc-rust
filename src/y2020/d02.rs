bb957778d08514a8803dc0beed88302e2bcdb758
Fedya Levkin <f.levkin@gmail.com>
2023-04-02 17:26:03 +0300

    Parsing PolicyAndPassword from string

---
+++ src/y2020/d02.rs
@@ -0,0 +1,58 @@
+use crate::file_reader::*;
+use std::str::FromStr;
+
+struct PolicyAndPassword {
+    required_letter: char,
+    min_count: usize,
+    max_count: usize,
+    the_password: String,
+}
+
+#[derive(Debug)]
+struct ParsingError;
+
+impl FromStr for PolicyAndPassword {
+    type Err = ParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split_whitespace().collect();
+
+        let range_parts: Vec<&str> = parts[0].split("-").collect();
+        let min_count = range_parts[0].parse::<usize>().unwrap();
+        let max_count = range_parts[1].parse::<usize>().unwrap();
+
+        let required_letter = parts[1].chars().next().unwrap();
+
+        let the_password = parts[2];
+
+        return Ok(PolicyAndPassword {
+            min_count,
+            max_count,
+            required_letter,
+            the_password: the_password.to_string(),
+        });
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pap_from_string() {
+        let string = "1-3 a: abcde";
+
+        let pap = PolicyAndPassword::from_str(string).unwrap();
+
+        assert_eq!(pap.required_letter, 'a');
+        assert_eq!(pap.min_count, 1);
+        assert_eq!(pap.max_count, 3);
+        assert_eq!(pap.the_password, "abcde");
+    }
+
+    #[test]
+    fn test_sample_answer_1() {
+        let input = get_string_from_file_please("ianda/2020/02/si.txt");
+        let expected = get_usize_from_file_please("ianda/2020/02/sa1.txt");
+        assert_eq!(2, expected)
+    }
+}

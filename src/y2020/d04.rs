e1c53fbbde2180926743d23d21bfbb5718dc7a02
Fedya Levkin <f.levkin@gmail.com>
2023-04-07 02:03:49 +0300

    y2020d04 solved, kinda oof, did stupid mistakes when testing

--- src/y2020/d04.rs
+++ src/y2020/d04.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use crate::file_reader::*;
 
 const REQUIRED_FIELDS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+const VALID_COLORS: [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
 
 fn get_input(path: &str) -> String {
     let input = get_string_from_file_please(path);
@@ -19,7 +20,6 @@ fn normalize_input(input: &str) -> String {
 }
 
 fn line_to_map(line: &str) -> HashMap<&str, &str> {
-    println!("line: '{}'", line);
     line.split(" ")
         .map(|s| s.split(":").collect::<Vec<&str>>())
         .map(|v| (v[0], v[1]))
@@ -35,11 +35,100 @@ fn validate_map(map: &HashMap<&str, &str>) -> bool {
     true
 }
 
+fn validate_map_full(map: &HashMap<&str, &str>) -> bool {
+    for field in REQUIRED_FIELDS.iter() {
+        if !map.contains_key(*field) {
+            return false;
+        }
+        let value = map.get(*field).unwrap();
+        match *field {
+            "byr" => {
+                let year = value.parse::<usize>().unwrap_or(0);
+                if year < 1920 || year > 2002 {
+                    return false;
+                }
+            }
+            "iyr" => {
+                let year = value.parse::<usize>().unwrap_or(0);
+                if year < 2010 || year > 2020 {
+                    return false;
+                }
+            }
+            "eyr" => {
+                let year = value.parse::<usize>().unwrap_or(0);
+                if year < 2020 || year > 2030 {
+                    return false;
+                }
+            }
+            "hgt" => {
+                let (height, unit) = value.split_at(value.len() - 2);
+                let height = height.parse::<usize>().unwrap_or(0);
+                match unit {
+                    "cm" => {
+                        if height < 150 || height > 193 {
+                            return false;
+                        }
+                    }
+                    "in" => {
+                        if height < 59 || height > 76 {
+                            return false;
+                        }
+                    }
+                    _ => {
+                        return false;
+                    }
+                }
+            }
+            "hcl" => {
+                if !value.starts_with("#") {
+                    return false;
+                }
+                let color = &value[1..];
+                if color.len() != 6 {
+                    return false;
+                }
+                for c in color.chars() {
+                    if !c.is_ascii_hexdigit() {
+                        return false;
+                    }
+                }
+            }
+            "ecl" => {
+                if !VALID_COLORS.contains(&value) {
+                    return false;
+                }
+            }
+            "pid" => {
+                if value.len() != 9 {
+                    return false;
+                }
+                for c in value.chars() {
+                    if !c.is_ascii_digit() {
+                        return false;
+                    }
+                }
+            }
+            _ => {
+                return false;
+            }
+        }
+    }
+    true
+}
+
 fn solve_task_1(filepath: &str) -> usize {
     get_input(filepath)
         .lines()
         .map(|l| line_to_map(l))
         .filter(|m| validate_map(m))
+        .count()
+}
+
+fn solve_task_2(filepath: &str) -> usize {
+    get_input(filepath)
+        .lines()
+        .map(|l| line_to_map(l))
+        .filter(|m| validate_map_full(m))
         .count()
 }
 
@@ -118,7 +207,7 @@ line4 line5";
 
     #[test]
     fn test_solve_task_1_sample() {
-        let result = solve_task_1("ianda/2020/04/si.txt");
+        let result = solve_task_1("ianda/2020/04/si1.txt");
         let expected = get_usize_from_file_please("ianda/2020/04/sa1.txt");
         assert_eq!(result, expected);
     }
@@ -130,11 +219,17 @@ line4 line5";
         assert_eq!(result, expected);
     }
 
-    /*
     #[test]
-    fn test_solve_task_2_sample() {
-        let result = solve_task_2("ianda/2020/04/si.txt");
+    fn test_solve_task_2_sample_2() {
+        let result = solve_task_2("ianda/2020/04/si2.txt");
         let expected = get_usize_from_file_please("ianda/2020/04/sa2.txt");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_solve_task_2_sample_3() {
+        let result = solve_task_2("ianda/2020/04/si3.txt");
+        let expected = get_usize_from_file_please("ianda/2020/04/sa3.txt");
         assert_eq!(result, expected);
     }
 
@@ -144,5 +239,4 @@ line4 line5";
         let expected = get_usize_from_file_please("ianda/2020/04/ra2.txt");
         assert_eq!(result, expected);
     }
-    */
 }

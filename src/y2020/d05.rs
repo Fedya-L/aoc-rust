0fdb2656bf53a2e9494ec9afcbf58b7ea3d0d277
Fedya Levkin <f.levkin@gmail.com>
2023-04-08 22:40:00 +0300

    y2020d05 solved

--- src/y2020/d05.rs
+++ src/y2020/d05.rs
@@ -1,6 +1,11 @@
+use std::collections::HashSet;
+
 use crate::file_reader::get_string_from_file_please;
 
-fn string_to_two_numbers(str: &str) -> (usize, usize) {
+#[derive(PartialEq, Eq, Debug, Hash)]
+struct Seat(usize, usize);
+
+fn string_to_seat(str: &str) -> Seat {
     let binary_numbers: String = str
         .chars()
         .map(|c| match c {
@@ -12,39 +17,77 @@ fn string_to_two_numbers(str: &str) -> (usize, usize) {
         })
         .collect();
     let (b1, b2) = binary_numbers.split_at(7);
-    (
+    Seat(
         usize::from_str_radix(b1, 2).unwrap(),
         usize::from_str_radix(b2, 2).unwrap(),
     )
+}
+
+fn get_id_from_seat(seat: &Seat) -> usize {
+    seat.0 * 8 + seat.1
 }
 
 fn solve_task_1(filepath: &str) -> usize {
     let input = get_string_from_file_please(filepath);
     input
         .lines()
-        .map(|l| string_to_two_numbers(l))
-        .map(|ns| ns.0 * 8 + ns.1)
+        .map(|l| string_to_seat(l))
+        .map(|ns| get_id_from_seat(&ns))
         .max()
         .unwrap()
 }
 
-mod d05_tests {
+fn solve_task_2(filepath: &str) -> usize {
+    let input = get_string_from_file_please(filepath);
+    let seats: HashSet<Seat> = HashSet::from_iter(input.lines().map(|l| string_to_seat(l)));
+
+    let mut saw_full_row = false;
+    for row in 0..128 {
+        for col in 0..8 {
+            if seats.contains(&Seat(row, col)) {
+                print!("#");
+                if saw_full_row == false && col == 7 {
+                    saw_full_row = true;
+                }
+            } else {
+                print!("x");
+                if saw_full_row {
+                    return get_id_from_seat(&Seat(row, col));
+                } else {
+                    break;
+                }
+            }
+        }
+        print!("\n");
+    }
+    0
+}
+mod tests {
     use crate::file_reader::get_usize_from_file_please;
 
     use super::*;
 
     #[test]
+    fn test_solve_task_2_input() {
+        let result = solve_task_2("ianda/2020/05/ri.txt");
+
+        let expected = get_usize_from_file_please("ianda/2020/05/ra1.txt");
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
     fn test_string_to_two_numbers_min() {
-        let result = string_to_two_numbers("FFFFFFFLLL");
-        let expected = (0usize, 0usize);
+        let result = string_to_seat("FFFFFFFLLL");
+        let expected = Seat(0usize, 0usize);
 
         assert_eq!(result, expected);
     }
 
     #[test]
     fn test_string_to_two_numbers_max() {
-        let result = string_to_two_numbers("BBBBBBBRRR");
-        let expected = (127usize, 7usize);
+        let result = string_to_seat("BBBBBBBRRR");
+        let expected = Seat(127usize, 7usize);
 
         assert_eq!(result, expected);
     }

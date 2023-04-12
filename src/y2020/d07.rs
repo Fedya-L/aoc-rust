0ad265eeb4b1cd0c898fb6736bd4d038bd7d8fdb
Fedya Levkin <f.levkin@gmail.com>
2023-04-13 00:05:02 +0300

    line_to_rule is here!

--- src/y2020/d07.rs
+++ src/y2020/d07.rs
@@ -6,20 +6,44 @@ struct Rule {
 }
 
 fn line_to_rule(line: &str) -> Rule {
-    let split = line.split(" contain ");
+    let (color, raw_options) = line.split_once(" bags contain ").unwrap();
+
+    let raw_options = raw_options.split(", ");
+    let options = raw_options
+        .map(|o| {
+            let mut split = o.split_ascii_whitespace();
+            let count = split.next().unwrap_or("0").parse::<usize>().unwrap_or(0);
+            _ = split.next_back();
+            let color = split.collect::<Vec<&str>>().join(" ");
+            Option(color, count)
+        })
+        .collect::<Vec<Option>>();
 
     Rule {
-        color: "CoLoUR".to_string(),
-        options: Vec::new(),
+        color: color.to_string(),
+        options: options,
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::line_to_rule;
 
     #[test]
     fn test_line_to_rule() {
         let line = "mirrored tomato bags contain 3 light yellow bags, 4 dim indigo bags, 5 light beige bags, 3 posh indigo bags.";
+
+        let rule = line_to_rule(line);
+
+        assert_eq!(rule.color, "mirrored tomato");
+        assert_eq!(rule.options[0].0, "light yellow");
+        assert_eq!(rule.options[0].1, 3);
+        assert_eq!(rule.options[1].0, "dim indigo");
+        assert_eq!(rule.options[1].1, 4);
+        assert_eq!(rule.options[2].0, "light beige");
+        assert_eq!(rule.options[2].1, 5);
+        assert_eq!(rule.options[3].0, "posh indigo");
+        assert_eq!(rule.options[3].1, 3);
     }
 
     #[test]
